1ddc97eaad3b68fe509157845478a53705f67500
Harry Kao <harry.kao@rockrabbit.ai>
2025-02-14 21:38:34 -0800

    Fix malformed TS when const value contains type.
    Prevent constant strings containing "number" and "boolean" from being
    treated like numbers and booleans. This was causing the value to appear in
    the TS without quotes.

--- src/schema-parser/base-schema-parsers/enum.ts
+++ src/schema-parser/base-schema-parsers/enum.ts
@@ -69,6 +69,9 @@ export class EnumSchemaParser extends MonoSchemaParser {
       if (value === null) {
         return this.config.Ts.NullValue(value);
       }
+      if (this.schemaUtils.isConstantSchema(this.schema)) {
+        return keyType;
+      }
       if (
         keyType.includes(this.schemaUtils.getSchemaType({ type: "number" }))
       ) {
